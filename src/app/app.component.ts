cff9f2e8ed8705bfe156dad77fa66cd09acd7053
Rafael <ralf_teles@hotmail.com>
2022-04-08 11:20:54 -0300

    novo

---
+++ src/app/app.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  dictStocks = {
+    "ITSA4": {
+      startValue: 20.0,
+      currentValue: 20.0,
+      change: 0.0
+    },
+    "TAEE11": {
+      startValue: 20.0,
+      currentValue: 20.0,
+      change: 0.0
+    },
+    "PETR4": {
+      startValue: 20.0,
+      currentValue: 20.0,
+      change: 0.0
+    }
+  };
+  dataConexao: any;
+
+  private _hubConnection: HubConnection;
+  
+  constructor() {  
+    this.createConnection();  
+    this.registerOnServerEvents();  
+    this.startConnection();  
+  }  
+  
+  connectToStock(symbol: string) {  
+    this._hubConnection.invoke('ConnectToStock', symbol);  
+  }  
+  
+  private createConnection() {  
+    this._hubConnection = new HubConnectionBuilder()  
+      .withUrl('https://localhost:5001/brokerhub')  
+      .build();  
+  }  
+  
+  private startConnection(): void {  
+    this._hubConnection  
+      .start()  
+      .then(() => {   
+        for (let key in this.dictStocks) {
+          this.connectToStock(key);
+        }
+      })  
+      .catch(() => {  
+        setTimeout(function () { this.startConnection(); }, 5000);  
+      });  
+  }  
+  
+  private registerOnServerEvents(): void {  
+    this._hubConnection.on('UpdatePrice', (data: any) => {
+
+      this.dataConexao = data.dataConexao;
+      let item = this.dictStocks[data.symbol];
+      item.currentValue = data.price;
+      item.change = (item.currentValue - item.startValue)/item.startValue;
+    });  
+  }  
+}
